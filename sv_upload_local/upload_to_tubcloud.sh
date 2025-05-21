db38704542eaf5eaa9d4e235cf3713f73df047b6
Mohamed Abu-Shawish <abu-shawish@campus.tu-berlin.de>
2025-05-21 13:15:02 +0200

    added shell script for tubcloud upload

--- sv_upload_local/upload_to_tubcloud.sh
+++ sv_upload_local/upload_to_tubcloud.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Lokales Verzeichnis (anpassen!)
-SRC="/home/deinuser/Master-Thesis/sv_upload_local/Data"
+# Lokales Verzeichnis
+SRC="$HOME/Master-Thesis/sv_upload_local/Data"
 
-# Zielpfad in der tuBCloud (z. B. ein Ordner namens Thesis-Backup)
+# Zielverzeichnis auf tuBCloud (z. B. "Master Thesis Data" – anpassen, falls du etwas anderes willst)
 DEST="tubcloud:Master Thesis Data"
 
-# Hochladen
-rclone copy "$SRC" "$DEST" --log-file=/var/log/rclone-tubcloud.log --create-empty-src-dirs --verbose
+# Log-Datei (optional)
+LOGFILE="$HOME/Master-Thesis/sv_upload_local/rclone_upload.log"
+
+# Upload ausführen
+rclone copy "$SRC" "$DEST" --log-file="$LOGFILE" --create-empty-src-dirs --verbose
